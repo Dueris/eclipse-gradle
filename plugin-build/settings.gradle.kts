44fed642fc7eeb7eb30faae184fcde04270a13cd
Dueris <122416109+Dueris@users.noreply.github.com>
2024-10-03 13:29:48 -0700

    Initial commit

---
+++ plugin-build/settings.gradle.kts
@@ -0,0 +1,36 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+
+    versionCatalogs {
+        create("libs") {
+            from(files("../gradle/libs.versions.toml"))
+        }
+    }
+}
+
+plugins {
+	id("com.gradle.develocity") version "3.18.1"
+}
+
+develocity {
+	buildScan.termsOfUseUrl = "https://gradle.com/terms-of-service"
+	buildScan.termsOfUseAgree = "yes"
+	buildScan.publishing.onlyIf {
+		System.getenv("GITHUB_ACTIONS") == "true" &&
+				it.buildResult.failures.isNotEmpty()
+	}
+}
+
+rootProject.name = ("com.ncorti.kotlin.gradle.template")
+
+include(":plugin")
---
+++ settings.gradle.kts
@@ -0,0 +1,31 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        mavenCentral()
+        google()
+    }
+}
+
+plugins {
+	id("com.gradle.develocity") version "3.18.1"
+}
+
+develocity {
+	buildScan.termsOfUseUrl = "https://gradle.com/terms-of-service"
+	buildScan.termsOfUseAgree = "yes"
+	buildScan.publishing.onlyIf {
+		System.getenv("GITHUB_ACTIONS") == "true" &&
+				it.buildResult.failures.isNotEmpty()
+	}
+}
+
+rootProject.name = "kotlin-gradle-plugin-template"
+
+include(":example")
+includeBuild("plugin-build")
