a77e8ebedc9f0f256482a561cfb7d262d79f16cc
Dueris <jedimastertoothless@hotmail.com>
2024-11-24 09:30:53 -0800

    add 1.21.3

--- plugin-build/plugin/src/main/java/io/github/dueris/kotlin/eclipse/gradle/MinecraftVersion.kt
+++ plugin-build/plugin/src/main/java/io/github/dueris/kotlin/eclipse/gradle/MinecraftVersion.kt
@@ -1,5 +1,6 @@
 package io.github.dueris.kotlin.eclipse.gradle
 
 enum class MinecraftVersion(val stringed: String) {
-    MC1_21_1("1.21-R0.1-SNAPSHOT")
+    MC1_21_1("1.21-R0.1-SNAPSHOT"),
+    MC1_21_3("1.21.3-R0.1-SNAPSHOT")
 }
